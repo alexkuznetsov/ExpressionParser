6ac1fe0e446005a87faed2dac23e47eee1983ca8
Alexandr Kuznetsov <kuznetsovshurik@gmail.com>
2020-05-21 23:33:04 +0600

    Убрал двойную проверку

--- ExpressionParser/Linq/MemberAccessExpressionParser.cs
+++ ExpressionParser/Linq/MemberAccessExpressionParser.cs
@@ -14,37 +14,18 @@ namespace ExpressionParser.Linq
             this.expression = expression;
         }
 
-        //private Expression NodeInBottom()
-        //{
-        //    var e = expression.Expression;
-        //    while(e.NodeType != ExpressionType.Parameter || e!=null)
-        //    {
-        //        e = E
-        //    }
-
-        //    return e;
-        //}
-
         public override Node Parse(IQueryMapping queryMapping)
         {
             var memberName = expression.GetPropertyName('.');
-            //if (expression.Expression.NodeType == ExpressionType.MemberAccess)
-            //{
-            //    var subNode = (MemberAccessNode)GetParser(expression.Expression).Parse(queryMapping);
-            //    memberName = subNode.MemberName + memberName;
-            //}
 
             if (expression.Expression is ConstantExpression)
             {
-                if (expression.Expression.NodeType == ExpressionType.Constant)
-                {
-                    var objectMember = Expression.Convert(expression, typeof(object));
-                    var getterLambda = Expression.Lambda<Func<object>>(objectMember);
-                    var getter = getterLambda.Compile();
-                    var targetValue = getter();
+                var objectMember = Expression.Convert(expression, typeof(object));
+                var getterLambda = Expression.Lambda<Func<object>>(objectMember);
+                var getter = getterLambda.Compile();
+                var targetValue = getter();
 
-                    return new ConstantNode(expression.Type, targetValue);
-                }
+                return new ConstantNode(expression.Type, targetValue);
             }
 
             if (!queryMapping.Mappings.TryGetValue(memberName, out var replacement))
