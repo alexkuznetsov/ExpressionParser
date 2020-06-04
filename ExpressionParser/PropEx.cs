a8c1ba5589ae08a874ede5959d8a51fbfa3c7a64
Alexandr Kuznetsov <kuznetsovshurik@gmail.com>
2020-06-04 19:36:04 +0600

    Fix mapping name autodetection

--- ExpressionParser/PropEx.cs
+++ ExpressionParser/PropEx.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq.Expressions;
+﻿using System;
+using System.Linq.Expressions;
 
 namespace ExpressionParser
 {
@@ -6,7 +7,10 @@ namespace ExpressionParser
     {
         public static string GetPropertyName/*<TModel, TValue>*/(this Expression/*<Func<TModel, TValue>>*/ propertySelector, char delimiter = '.', char endTrim = ')')
         {
-
+            while (propertySelector.NodeType != ExpressionType.MemberAccess)
+            {
+                propertySelector = PopMemberAccess(propertySelector);
+            }
             var asString = propertySelector.ToString(); // gives you: "o => o.Whatever"
             var firstDelim = asString.IndexOf(delimiter); // make sure there is a beginning property indicator; the "." in "o.Whatever" -- this may not be necessary?
 
@@ -14,5 +18,19 @@ namespace ExpressionParser
                 ? asString
                 : asString.Substring(firstDelim + 1).TrimEnd(endTrim);
         }//--   fn  GetPropertyNameExtended
+
+        private static Expression PopMemberAccess(Expression expression)
+        {
+            switch(expression.NodeType)
+            {
+                case ExpressionType.Lambda:
+                    return ((LambdaExpression)expression).Body;
+                case ExpressionType.Convert:
+                    return ((UnaryExpression)expression).Operand;
+
+                default:
+                    throw new NotSupportedException();
+            }
+        }
     }
 }
